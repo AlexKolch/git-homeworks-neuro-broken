508bbb39a6907c67b0f00e227a36017c5d84ca86
Alexey Kolch <kolychenkov94@mail.ru>
2022-11-02 00:27:02 +0400

    add feature/earlyorder

--- js/app.js
+++ js/app.js
@@ -1,30 +1,20 @@
 (() => {
-    const subscriptionForm = document.querySelector('#subscription-form');
+    const earlyorderForm = document.querySelector('#earlyorder-form');
 
-    subscriptionForm.addEventListener('submit', ev => {
+    earlyorderForm.addEventListener('submit', ev => {
         ev.preventDefault();
 
-        const subpscriptionEmail = subscriptionForm.querySelector('#subscription-email');
-        const subpscriptionHelp = subscriptionForm.querySelector('#subscription-help');
-        const email = subpscriptionEmail.value.trim();
+        const earlyorderContact = earlyorderForm.querySelector('#earlyorder-contact');
+        const contact = earlyorderContact.value.trim();
 
-        if (email === '') {
-            subpscriptionHelp.classList.remove('text-hide');
-            return;
-        }
-
-        const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        if (re.matches(email)) {
-            subpscriptionHelp.classList.remove('text-hide');
+        if (contact === '') {
             return;
         }
         
-        subpscriptionHelp.classList.add('text-hide');
-
         // TODO: send email simulation:
         setTimeout(() => {
-            alert('Вы успешно подписаны на новости');
-            subpscriptionEmail.value = '';
+            $('#earlyorderModal').modal('show');
+            earlyorderContact.value = '';
         }, 500);
     });
 })();
